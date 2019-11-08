0ce6db5582420557fe424f73aeb212655a7fd444
Erlend Vige <erlend.vige@gmail.com>
2019-11-08 16:24:49 +0100

    Merge

--- packages/@navikt/application-wrapper/applicationWrapper.tsx
+++ packages/@navikt/application-wrapper/applicationWrapper.tsx
@@ -10,12 +10,18 @@ const ApplicationWrapper: React.FunctionComponent = ({ children }) => (
         userUnit="NFP Drammen"
         renderUserPopoverContent={() => (
             <BoxedListWithSelection
-                items={[{ name: 'Test 1', href: 'nav.no', selected: true }, { name: 'Test 2', href: 'localhost:1234' }]}
+                items={[
+                    { name: 'Test 1', href: 'nav.no', selected: true },
+                    { name: 'Test 2', href: 'localhost:1234' },
+                ]}
             />
         )}
         renderLinksPopoverContent={() => (
             <BoxedListWithLinks
-                items={[{ name: 'Test 1', href: 'nav.no' }, { name: 'Test 2', href: 'localhost:1234' }]}
+                items={[
+                    { name: 'Test 1', href: 'nav.no' },
+                    { name: 'Test 2', href: 'localhost:1234' },
+                ]}
             />
         )}
     />
