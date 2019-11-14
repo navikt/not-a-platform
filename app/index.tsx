da9256cebcc86daa6f6ed4e873e133ec7d16cb82
Erlend Vige <erlend.vige@gmail.com>
2019-11-14 17:57:15 +0100

    Remove old /app folder

--- app/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import ApplicationWrapper from '../packages/@navikt/application-wrapper/applicationWrapper';
-import 'normalize.css';
-
-render(<ApplicationWrapper>Hello World</ApplicationWrapper>, document.getElementById('app'));
