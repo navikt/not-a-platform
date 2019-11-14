0f0dcf422e372023654f952c483b8cbc8a8a9cba
Hallvard Andreas Stark <hallvard@scel.to>
2019-11-14 09:22:20 +0100

    added header with popup story

--- packages/@navikt/header/src/Header.stories.tsx
+++ packages/@navikt/header/src/Header.stories.tsx
@@ -4,3 +4,11 @@ import Header from './index';
 export default { title: '@navikt/header' };
 
 export const normal = () => <Header title="Svangerskap og Fødsel" userName="Navn Brukersen" userUnit="NFP Drammen" />;
+export const withUserPopover = () => (
+    <Header
+        title="Svangerskap og Fødsel"
+        userName="Navn Brukersen"
+        userUnit="NFP Drammen"
+        renderUserPopoverContent={(): React.ReactNode => <div>Test</div>}
+    />
+);
