82340823397c23296806f7eca6b61c98bab7fa5d
Hallvard Andreas Stark <hallvard@scel.to>
2019-11-15 12:44:33 +0100

    Jest with automatic snapshot generation of stories

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    roots: ['<rootDir>/packages'],
+    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(ts|tsx|js)?$': 'babel-jest',
+        '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    },
+    moduleNameMapper: {
+        '.*\\.(css|less)$': '<rootDir>/styleMock.js',
+    },
+    moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node', 'less'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    transformIgnorePatterns: ['<rootDir>.*(node_modules)(?!.*nav.*).*$'],
+};
