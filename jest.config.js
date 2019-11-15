0d999cb3da7b0fc01ec587f209b887a0ef3a03da
Hallvard Andreas Stark <hallvard@scel.to>
2019-11-15 14:37:57 +0100

    moved tests and mocks folders to root

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    roots: ['<rootDir>/packages'],
-    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    roots: ['<rootDir>'],
+    testMatch: ['**/__tests__/**/*.+(ts|tsx)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
     transform: {
         '^.+\\.(ts|tsx|js)?$': 'babel-jest',
         '^.+.(css|less)$': 'jest-transform-stub',
         '^.+\\.stories\\.tsx?$': '@storybook/addon-storyshots/injectFileName',
     },
     moduleNameMapper: {
-        '.*\\.(css|less)$': '<rootDir>/packages/__mocks__/styleMock.js',
+        '.*\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     },
     moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'less'],
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
