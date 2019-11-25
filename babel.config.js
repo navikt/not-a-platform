8a46bf5b4e74be155d5f7942d351829ea4ccfaba
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2019-11-25 09:29:02 +0100

    added babel plugins for optional chaining (#23)
    and nullish coalescing operator

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript', '@babel/react'],
-    plugins: ['macros'],
+    plugins: [
+        'macros',
+        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+    ],
 };
