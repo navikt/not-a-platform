9539b3b76d38121422d020addf65d405e0b34ed5
Hallvard Andreas Stark <hallvard@scel.to>
2019-12-04 14:57:19 +0100

    la til snapshot og screenshot

--- packages/stories/person-card.stories.tsx
+++ packages/stories/person-card.stories.tsx
@@ -3,7 +3,7 @@ import PersonCard from '../@navikt/person-card/src/index';
 
 export default { title: '@navikt/nap-person-card' };
 
-export const StateFromProps = (): JSX.Element => {
+const StateFromProps = (): JSX.Element => {
     const [activeIndex, setActiveIndex] = React.useState(0);
 
     return (
@@ -27,3 +27,4 @@ export const StateFromProps = (): JSX.Element => {
         />
     );
 };
+export const stateFromProps = (): React.ReactNode => <StateFromProps />;
