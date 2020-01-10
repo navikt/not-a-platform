be9535b74c31166b8987fa70170ce67452e5b1f1
Hallvard Andreas Stark <hallvard@scel.to>
2020-01-10 12:09:58 +0100

    la inn bedre ikon for clipboard

--- packages/@navikt/clipboard/src/ClipboardIcon.tsx
+++ packages/@navikt/clipboard/src/ClipboardIcon.tsx
@@ -28,12 +28,17 @@ const svgForType = (type): JSX.Element => {
             );
         case 'copy':
             return (
-                <g strokeLinecap="round" strokeLinejoin="round" {...defaultIconProps}>
-                    <g strokeWidth="2">
-                        <g transform="translate(4, 4)">
-                            <polygon points="4.4408921e-14 19.1729323 4.4408921e-14 4.5112782 10.1503759 4.5112782 10.1503759 19.1729323" />
-                            <polyline points="5.63909774 2.19924812 5.63909774 -2.69118061e-13 15.7894737 -2.69118061e-13 15.7894737 14.6616541 13.2518797 14.6616541" />
-                        </g>
+                <g
+                    stroke="none"
+                    strokeWidth="1"
+                    fill="none"
+                    fillRule="evenodd"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <g transform="translate(4.000000, 2.000000)" stroke="#3E3832" strokeWidth="1.5">
+                        <polygon points="4.4408921e-14 19.1729323 4.4408921e-14 4.5112782 10.1503759 4.5112782 10.1503759 19.1729323" />
+                        <polyline points="5.63909774 2.19924812 5.63909774 -2.69118061e-13 15.7894737 -2.69118061e-13 15.7894737 14.6616541 13.2518797 14.6616541" />
                     </g>
                 </g>
             );
@@ -44,7 +49,7 @@ const svgForType = (type): JSX.Element => {
 
 const ClipboardIcon = ({ size = 24, type }: ClipboardIconProps): JSX.Element => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="-2 0 26 26">
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24">
             {svgForType(type)}
         </svg>
     );
