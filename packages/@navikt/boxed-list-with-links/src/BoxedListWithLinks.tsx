c0b3b7c7d0a06faff140b5dd3f793f4044ca9579
Erlend Vige <erlend.vige@gmail.com>
2020-01-31 11:00:46 +0100

    Refactor BoxedListWith-components, add onClick (#38)
    Adds onclick support to boxedListWithLinks
    Remove href-prop / anchor-rendering from BoxedListWithSelection
    Update stories and versions

--- packages/@navikt/boxed-list-with-links/src/BoxedListWithLinks.tsx
+++ packages/@navikt/boxed-list-with-links/src/BoxedListWithLinks.tsx
@@ -9,13 +9,15 @@ interface ListItemProps {
     name: string;
     /** Url for link */
     href: string;
-    /** Is this an external url? */
+    /** Whether to open link in a new window */
     isExternal?: boolean;
 }
 
 interface BoxedListWithLinksProps {
     /** Array of links */
-    items?: ListItemProps[];
+    items: ListItemProps[];
+    /** Onclick handler */
+    onClick?: (index: number, e: React.SyntheticEvent) => void;
 }
 
 const listItemCls = bem('boxedList__item');
@@ -23,15 +25,19 @@ const listItemCls = bem('boxedList__item');
 /** Bordered list with links */
 export const BoxedListWithLinks: React.FunctionComponent<BoxedListWithLinksProps> = ({
     items,
+    onClick,
 }: BoxedListWithLinksProps) => (
     <BoxedList>
-        {items.map(({ name, href, isExternal }) => (
+        {items.map(({ name, href, isExternal }, index) => (
             <li className={listItemCls.block} key={href}>
                 <a
                     href={href}
                     className={listItemCls.element('anchor')}
                     target={isExternal ? '_blank' : ''}
                     rel={isExternal ? 'noopener' : ''}
+                    onClick={e => {
+                        if (onClick) onClick(index, e);
+                    }}
                 >
                     <Normaltekst>{name}</Normaltekst>
                 </a>
