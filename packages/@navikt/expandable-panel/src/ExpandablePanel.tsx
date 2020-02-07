e543e93c4ae0919995c40a01c542c99963002daf
Hallvard Andreas Stark <hallvard@scel.to>
2020-02-07 14:20:05 +0100

    laget første versjon av expandable panel

--- packages/@navikt/expandable-panel/src/ExpandablePanel.tsx
+++ packages/@navikt/expandable-panel/src/ExpandablePanel.tsx
@@ -1,22 +1,37 @@
 import * as React from 'react';
-import styles from './expandablePanelStyles';
+import { Collapse } from 'react-collapse';
 import bem from '../../bem-utils';
+import './expandablePanelStyles';
 
 const expandablePanelCls = bem('expandablePanel');
 
 type ExpandablePanelTheme = 'success' | 'warn' | 'neutral';
 
 interface ExpandablePanelProps {
-    theme: ExpandablePanelTheme;
-    children: React.ReactChildren;
+    theme?: ExpandablePanelTheme;
+    children: React.ReactChild | React.ReactChildren;
     renderHeader: () => React.ReactNode;
 }
 
-const ExpandablePanel: React.FunctionComponent<ExpandablePanelProps> = ({ renderHeader, children }) => (
-    <div>
-        <button type="button">{renderHeader()}</button>
-        <div>{children}</div>
-    </div>
-);
+const ExpandablePanel: React.FunctionComponent<ExpandablePanelProps> = ({ renderHeader, children, theme }) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    return (
+        <div className={expandablePanelCls.block}>
+            <div className={theme ? expandablePanelCls.elementWithModifier('themeBorder', theme) : ''} />
+            <div className={expandablePanelCls.element('contentWrapper')}>
+                <button
+                    className={expandablePanelCls.element('button')}
+                    type="button"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    {renderHeader()}
+                </button>
+                <Collapse isOpened={isOpen}>
+                    <div className={expandablePanelCls.element('expandedContent')}>{children}</div>
+                </Collapse>
+            </div>
+        </div>
+    );
+};
 
 export default ExpandablePanel;
---
+++ packages/stories/ExpandablePanel.stories.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import ExpandablePanel from '../@navikt/expandable-panel/src';
+import 'normalize.css';
+
+export default { title: '@navikt/nap-expandable-panel' };
+
+export const propsControlled = (): React.ReactNode => (
+    <div style={{ margin: '1rem' }}>
+        <div style={{ margin: '1rem 0' }}>
+            <ExpandablePanel renderHeader={() => <p>Noe tekst her</p>} theme="success">
+                <p>Hei du</p>
+            </ExpandablePanel>
+        </div>
+        <div style={{ margin: '1rem 0' }}>
+            <ExpandablePanel renderHeader={() => <p>Noe tekst her</p>} theme="warn">
+                <p>Hei du</p>
+            </ExpandablePanel>
+        </div>
+    </div>
+);
